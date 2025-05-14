619a3d8ca2aaf1c9f8a7321767a79bfb77ffa9eb
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 10:53:34 -0300

    FEAT - Importação do swagger no index.js da API

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', routes);
 
