50b565ca77be248cf3d4fb2acccf0f6b44b07fc7
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 11:47:05 -0300

    FEAT - Adição de parametro lang na paginação

--- src/schemas/paginacaoSchema.js
+++ src/schemas/paginacaoSchema.js
@@ -3,6 +3,17 @@ const Joi = require('joi');
 const paginacaoSchema = Joi.object({
   pagina: Joi.number().integer().min(1).default(1),
   limite: Joi.number().integer().min(1).max(100).default(5),
+  lang: Joi.string()
+    .trim()
+    .min(1)
+    .valid('en', 'pt')
+    .default('en')
+    .messages({
+      'any.only': 'Idioma inválido. Os valores aceitos são: en, pt.',
+      'string.base': 'O idioma deve ser uma string.',
+      'string.empty': 'O idioma não pode estar vazio.',
+      'string.min': 'O idioma não pode estar vazio.'
+    }),
 });
 
 module.exports = { paginacaoSchema };
