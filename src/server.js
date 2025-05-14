f7bfc924f299deda247a07fb483e698c1ffac89c
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 12:48:46 -0300

    FIX - Correção do PORT no console.log em server.js

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
