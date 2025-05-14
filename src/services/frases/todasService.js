7ab94ff7519a7197d29b78bff42084d3e891156a
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 11:48:45 -0300

    FEAT - Adição de parametro lang e aviso na res em todasService.js

--- src/services/frases/todasService.js
+++ src/services/frases/todasService.js
@@ -1,18 +1,24 @@
 const { carregarFrases } = require('../../utils/carregarFrasesUtil');
 
-async function listarTodasAsFrases(pagina = 1, limite = 5) {
+async function listarTodasAsFrases(pagina = 1, limite = 5, lang = 'en') {
   const frases = await carregarFrases();
 
   const inicio = (pagina - 1) * limite;
   const fim = inicio + limite;
   const paginadas = frases.slice(inicio, fim);
 
-  return {
+  const resposta = {
     total: frases.length,
     pagina,
     limite,
     resultados: paginadas
   };
+
+  if (lang !== 'en') {
+    resposta.aviso = "Tradução automática de frases em lote está desativada por motivos de desempenho. Frases retornadas em inglês.";
+  }
+
+  return resposta;
 }
 
 module.exports = { listarTodasAsFrases };
