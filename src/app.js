64c3c345fe37aaa9401d716abae7432a4f9fc4ec
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 11:17:45 -0300

    REFACT - Refatoração do index.js da API para app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./docs/swagger.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, 'docs', 'swagger.yaml'));
 
 const app = express();
 
@@ -12,10 +13,13 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Rotas da API
 app.use('/', routes);
 
+// Rota 404
 app.use((req, res) => {
   res.status(404).json({ erro: 'Rota não encontrada' });
 });
