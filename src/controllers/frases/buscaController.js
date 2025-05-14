5ec91b68f2fc2ed2214c107961e4ff14abb10c7b
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 12:39:55 -0300

    FEAT - Inclusão do aviso no retorno do buscaController

--- src/controllers/frases/buscaController.js
+++ src/controllers/frases/buscaController.js
@@ -4,13 +4,16 @@ async function buscarFrasesPorPalavraChaveController(req, res) {
   const palavra = req.params.palavra?.toLowerCase();
 
   try {
-    const resultados = await buscarFrasesPorPalavraChave(palavra);
+    const { aviso, resultados } = await buscarFrasesPorPalavraChave(palavra);
 
     if (resultados.length === 0) {
-      return res.status(404).json({ erro: `Nenhuma frase contém a palavra: ${req.params.palavra}` });
+      return res.status(404).json({
+        erro: `Nenhuma frase contém a palavra: ${req.params.palavra}`,
+        aviso
+      });
     }
 
-    return res.status(200).json(resultados);
+    return res.status(200).json({ aviso, resultados });
   } catch (err) {
     console.error('Erro ao buscar frases por palavra:', err);
     return res.status(500).json({ erro: 'Erro interno ao buscar frases por palavra.' });
