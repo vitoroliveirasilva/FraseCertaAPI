1b9117bcc37436db083f79efce9c9951f5a517fa
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-14 11:47:21 -0300

    FEAT - Adição de mensagens de erro no paginacaoSchema.js

--- src/schemas/paginacaoSchema.js
+++ src/schemas/paginacaoSchema.js
@@ -1,8 +1,26 @@
 const Joi = require('joi');
 
 const paginacaoSchema = Joi.object({
-  pagina: Joi.number().integer().min(1).default(1),
-  limite: Joi.number().integer().min(1).max(100).default(5),
+  pagina: Joi.number()
+    .integer()
+    .min(1)
+    .default(1)
+    .messages({
+      'number.base': 'O número da página deve ser um número inteiro.',
+      'number.integer': 'A página deve ser um número inteiro.',
+      'number.min': 'A página deve ser no mínimo 1.'
+    }),
+  limite: Joi.number()
+    .integer()
+    .min(1)
+    .max(100)
+    .default(5)
+    .messages({
+      'number.base': 'O limite deve ser um número inteiro.',
+      'number.integer': 'O limite deve ser um número inteiro.',
+      'number.min': 'O limite deve ser no mínimo 1.',
+      'number.max': 'O limite deve ser no máximo 100.'
+    }),
   lang: Joi.string()
     .trim()
     .min(1)
